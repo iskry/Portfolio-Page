2749be52032458283ef4f06493fa16f96b9d789c
iskry <refocuslucifer@protonmail.com>
2023-01-10 23:48:47 -0600

    redid portfolio. added recent projects

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-var navLinks = document.querySelectorAll(".nav-link")
-console.log(navLinks)
-
-
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function() {
-      navLinks[i].classList.toggle("active");
-    });
-}
